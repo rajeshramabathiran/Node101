0c5b24b36f269bd0df2ca5b8b37ab7fe7d8a4a30
Rajesh Ramabathiran <rajram@microsoft.com>
2017-03-18 14:57:23 -0700

    Added support for mongodb
    Currently it works only for local mongoldb. Code will be updated to
    support connection string provided via environment variable.

--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -1,13 +1,19 @@
 import { Get, Route } from "tsoa";
 import { Book } from "../models/book";
+import { BooksDB } from "../utils/booksDB";
 
 @Route('books')
 export class BooksController {
     @Get()
     public getBooks(): Promise<Book[]> {
-        let books: Book[] = [
-            new Book("Harry Potter", "J.K. Rowling")
-        ];
-        return Promise.resolve(books);
+        let connectionString: string = "mongodb://localhost/booksDB";
+        let booksDB: BooksDB = new BooksDB(connectionString);
+        return new Promise<Book[]>( (resolve, reject) => {
+            booksDB.getBooks().then(books => {
+                 resolve(books);
+             }).catch(error => {
+                 reject(error);
+             });
+        });
     }
 }
---
+++ src/utils/booksDB.ts
@@ -0,0 +1,67 @@
+import * as util from "util";
+import * as mongodb from "mongodb";
+import { MongoClient } from "mongodb";
+import { Book } from "../models/Book";
+
+export class BooksDB {
+    private mongodbClient: MongoClient;
+    private db: mongodb.Db;
+    private connectionString: string;
+
+    constructor(connectionString: string){
+        this.connectionString = connectionString;
+        this.mongodbClient = new MongoClient();
+    }
+
+    public connectDB(): Promise<mongodb.Db> {
+        return new Promise<mongodb.Db>((resolve, reject) => {
+            if(this.db) {
+                return this.db;
+            }
+            
+            this.mongodbClient.connect(this.connectionString, (error: mongodb.MongoError, _db: mongodb.Db) => {
+                if(error) {
+                    reject (error);
+                }
+                this.db = _db;
+                resolve(this.db);
+            })
+        });
+    }
+
+
+    public getBooks(): Promise<Book[]>
+    {
+        return this.connectDB().then(db => {
+            let collection: mongodb.Collection = db.collection('books');
+            return new Promise<Book[]>((resolve, reject) => {
+                let books: Book[] = [];
+                collection.find().toArray().then(docs => {
+                    docs.map((doc: Book) => {
+                        books.push(new Book(doc.name, doc.authorName));
+                    })
+                    resolve(books);
+                }).catch(error => {
+                    reject(error);
+                })
+            })
+        });
+    }
+
+    public insertBook(book: Book) : Promise<Book> {
+        return this.connectDB().then(db => {
+            let collection: mongodb.Collection = db.collection('books');
+            return new Promise<Book>((resolve, reject) => {
+                collection.insert(book).then(result => {
+                    resolve(book);    
+                }).catch(error => {
+                    reject(error);
+                });
+            })
+        });
+    }
+
+    public Close(): void {
+        this.db.close();
+    }
+}
