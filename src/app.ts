5080bb5bc583ec79d554e2c00162bea27b4b9640
Rajesh Ramabathiran <rajram@microsoft.com>
2017-03-18 23:02:53 -0700

    Added Swagger UI support in the base route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import * as bodyParser from "body-parser";
 import * as methodOverride from "method-override";
 import { RegisterRoutes} from "./routes/routes";
 import * as path from "path";
+import * as swaggerUI from "swagger-ui-express";
+
+const swaggerJSON = require('./swagger/swagger.json');
 
 let app: express.Application = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,14 +13,16 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 RegisterRoutes(app);
 
-let router = express.Router();
-router.get('/', (req, res, next) => {
-    res.send('Welcome to Node 101 tutorial! Navigate to /docs for API usage.');
-})
+// let router = express.Router();
+// router.get('/', (req, res, next) => {
+//     res.send('Welcome to Node 101 tutorial! Navigate to /api-docs for API usage.');
+// })
 
-app.use('/', router);
+// app.use('/', router);
 
 app.use('/swagger.json', express.static(__dirname + '/swagger/swagger.json'));
-app.use('/docs', express.static(__dirname + '/swagger-ui'));
+//app.use('/docs', express.static(__dirname + '/swagger-ui'));
+
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 
 export default app;
