dacb4404df3cd46b963c55cfeffea726dd67263e
Rajesh Ramabathiran <rajram@microsoft.com>
2017-03-19 00:10:13 -0700

    Added gulp tasks for build
    Removed VS Code task runners and replaced it with gulp task

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const swagger = ['src/swagger/swagger.json'];
+
+// pull in the project TypeScript config
+const tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('scripts', () => {
+  const tsResult = tsProject.src()
+  .pipe(tsProject());
+  return tsResult.js.pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['scripts'], () => {
+  gulp.watch('src/**/*.ts', ['scripts']);
+});
+
+gulp.task('swagger', function() {
+  return gulp.src(swagger)
+  .pipe(gulp.dest('dist/swagger'));
+});
+
+gulp.task('default', ['watch', 'swagger']);
